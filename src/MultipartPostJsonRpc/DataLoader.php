3082b6ff9affc7c673460553efde50615daa89c8
Andrey Tyurin <ectb08@mail.ru>
2018-10-22 22:28:38 +0300

    create data loader dev version

--- src/MultipartPostJsonRpc/DataLoader.php
+++ src/MultipartPostJsonRpc/DataLoader.php
@@ -4,8 +4,46 @@ namespace RpcExample\MultipartPostJsonRpc;
 
 
 class DataLoader implements \Oploshka\Rpc\iDataLoader {
+
+  private $filed ;
+  private $Reform;
+  
+  function  __construct(){
+    $this->filed  = 'data';
+    $this->Reform = new \Oploshka\Reform\Reform([]);
+  }
   
   public function load(&$methodName, &$methodData){
+  
+    // Request method is post
+    if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+      return 'ERROR_REQUEST_METHOD_TYPE';
+    }
+    // Post is empty
+    if($_POST == [] ) {
+      return 'ERROR_POST_NULL';
+    }
+    // $_POST['data']  not send
+    if( !isset($_POST[$this->filed]) ) {
+      return 'ERROR_POST_DATA_NULL';
+    }
+    // convert $_POST['data'] (json string) in array
+    $data = $this->Reform->item($_POST[$this->filed], ['type' => 'json']);
+    if ($data === NULL){
+      return 'ERROR_POST_DATA_JSON_DECODE_ERROR';
+    }
+  
+    // todo: id, jsonrpc
+    if( !isset($data['method']) ) {
+      return 'ERROR_NO_METHOD_NAME';
+    }
+    if( !isset($data['params']) ) {
+      return 'ERROR_NO_METHOD_PARAMS';
+    }
+
+    $methodName = $data['method'];
+    $methodData = $data['params'];
+    
     return 'ERROR_NOT';
   }
   
